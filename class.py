8af2ac6f18e30915b26a774bbd188af318e613ef
Brightsparks_26 <104679047+engrkel25054@users.noreply.github.com>
2025-08-28 17:59:42 +0400

    Add files via upload

---
+++ class.py
@@ -0,0 +1,65 @@
+class Int_set(object):
+    """An Int_set is a set of integers."""
+    def __init__(self):
+        """Create an empty set of integers."""
+        self._vals = []
+
+    def insert(self, e):
+        """Assumes e is an integer and inserts e into self."""
+        if e not in self._vals:
+            self._vals.append(e)
+
+    def member(self, e):
+        """Assumes e in an integer and returns True if e is in self, and False otherwise."""
+        return e in self._vals
+
+    def remove(self, e):
+        """
+        Assumes e is an integer and removes e from self.
+        Raises ValueError if e is not in self.
+        """
+        try:
+            self._vals.remove(e)
+        except:
+            raise ValueError(str(e) + "not found.")
+
+    def get_members(self):
+        """
+        Returns a list containing the elements of self.
+        Nothing can be assumed about the order of the elements.
+        """
+        return self._vals[:]
+
+    def __str__(self):
+        """Returns a string representation of self."""
+        if self._vals == []:
+            return "{}"
+        self._vals.sort()
+        result = ""
+
+        for e in self._vals:
+            result = result + str(e) + ", "
+
+        return f"{{{result[:-1]}}}"
+
+    def union(self, other):
+        """
+        other is an Int set; mutates self so that it contains exactly the elements in self;
+        plus the elements in other.
+        """
+        for e in other.get_members():
+            self._vals.append(e)
+
+
+s = Int_set()
+for i in range(22, 32, 2):
+    s.insert(i)
+print("The value of s is", s)
+
+o = Int_set()
+for i in range(5):
+    o.insert(i)
+print("The value of o is", o)
+
+o.union(s)
+print("The value of the union is", str(o))
